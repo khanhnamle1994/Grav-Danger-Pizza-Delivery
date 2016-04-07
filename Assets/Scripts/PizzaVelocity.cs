5baaded2a4c2751f1e9b4441d4b9d54b3caa853e
Wesley Lin <wesley.lin@vanderbilt.edu>
2016-04-07 11:04:38 +0200

    modified PizzaVelocity and PlanetGravity to use new rigidbody velocity rather than manually adjusting the transform.position

--- Assets/Scripts/PizzaVelocity.cs
+++ Assets/Scripts/PizzaVelocity.cs
@@ -4,20 +4,22 @@ using System.Collections;
 public class PizzaVelocity : MonoBehaviour {
 
 	public Vector2 initialVelocity;
-	Vector2 velocity;
+
+    Rigidbody2D rg2d;
 
 	// Use this for initialization
 	void Start () {
-		velocity = initialVelocity;
+        rg2d = GetComponent<Rigidbody2D>();
+        rg2d.velocity = initialVelocity;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		gameObject.transform.position += new Vector3(velocity.x, velocity.y);
+		//gameObject.transform.position += new Vector3(velocity.x, velocity.y);
 	}
 
 	public void AddForce(Vector2 newForce)
 	{
-		velocity += newForce;
+        rg2d.velocity += newForce;
 	}
 }
