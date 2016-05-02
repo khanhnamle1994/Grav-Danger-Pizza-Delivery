0d644491a3d5c7afa966972a356e7fc3f4c51a76
ballen3434 <benallen1423@gmail.com>
2016-05-02 16:59:27 +0200

    new asteroid
    rawr

---
+++ Assets/Scripts/AsteriodCollide.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteriodCollide : MonoBehaviour {
+
+   // public string item;
+    //public string item2;
+
+    //string[] items = new string[2]; 
+
+   void Start()
+    {
+        //items[0] = item;
+        //items[1] = item2; 
+    }
+
+   
+
+   void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            
+            other.GetComponent<PizzaInventory>().Decrement("Pizza"); 
+            //Destroy(gameObject);
+            
+        }
+    }
+}
