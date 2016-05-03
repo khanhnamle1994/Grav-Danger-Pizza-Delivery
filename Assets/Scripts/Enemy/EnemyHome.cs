686a979d5bd294a3f63c0d96296b12fd5f63067c
Wesley Lin <wesleylin625@gmail.com>
2016-05-03 23:38:34 +0200

    merge fix

--- Assets/Scripts/Enemy/EnemyHome.cs
+++ Assets/Scripts/Enemy/EnemyHome.cs
@@ -14,15 +14,9 @@ public class EnemyHome : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Debug.DrawLine (enemy.transform.position, home.transform.position);
-<<<<<<< HEAD
-		//Vector2 directionVector = OtherToParticleVector2(other);
-		//float gravityForce = home.GetComponent<Rigidbody2D>().mass * .1f;
-		//home.GetComponent<Rigidbody2D>().AddForce(directionVector.normalized * force,ForceMode2D.Force);
-=======
 		Vector2 directionVector = home.transform.position - enemy.transform.position;
 		float gravityForce = home.GetComponent<Rigidbody2D>().mass * 1f;
 		enemy.GetComponent<Rigidbody2D>().AddForce(directionVector.normalized * gravityForce, ForceMode2D.Force);
->>>>>>> e86d29fee4f8ae8ddfd8a7e93d8ca61b1c3ec45d
 	}
 
 	void OnTriggerEnter2D (Collider2D other) {
