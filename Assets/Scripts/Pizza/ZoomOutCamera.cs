76a0656f763744740f6265cc41640af6a377eceb
Gregory E Garcia <ge95garcia@gmail.com>
2016-05-03 16:30:25 +0200

    Added more audio

---
+++ Assets/Scripts/Pizza/ZoomOutCamera.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ZoomOutCamera : MonoBehaviour {
+
+	public GameObject background;
+	public int sceneOne;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (ZoomOut ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown(){
+		SceneManager.LoadScene(sceneOne);
+	}
+
+	IEnumerator ZoomOut()
+	{
+		Camera.main.orthographicSize += .015f;
+		background.transform.localScale += new Vector3 (.005f, .005f, 0);
+		yield return null;
+		if (Camera.main.orthographicSize < 35f)
+			StartCoroutine(ZoomOut ());
+		else
+			SceneManager.LoadScene(sceneOne);
+	}
+}
