6ace34183dbd1189984f3cbacce89931972aaa85
kevnburg <kevin.e.ellenburg@vanderbilt.edu>
2016-05-03 23:03:27 +0200

    Tutorial Audio Progress
    still more work to do

--- Assets/Scripts/TutorialAudioQueues.cs
+++ Assets/Scripts/TutorialAudioQueues.cs
@@ -3,13 +3,31 @@ using System.Collections;
 
 public class TutorialAudioQueues : MonoBehaviour {
 
+	AudioTrack audio;
+
 	// Use this for initialization
 	void Start () {
-	
+		audio = GetComponent<AudioTrack> ();
+		StartCoroutine (playNext());
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	}
+
+	IEnumerator playNext() {
+		audio.PlayNextWhenReady (); //left click to create blue particle
+		yield return new WaitUntil(()=> { return Input.GetMouseButtonDown(0); }); //wait until left click
+		audio.PlayNextWhenReady();
+		audio.PlayNextWhenReady ();
+		yield return new WaitUntil(()=> {return Input.GetMouseButtonDown(1); }); //wait until right click
+		audio.PlayNextWhenReady();
+		audio.PlayNextWhenReady ();
+		yield return new WaitUntil (()=> { return Input.GetMouseButtonDown(0) && Input.GetButton("Shift"); }); //wait until green
+		audio.PlayNextWhenReady();
+		audio.PlayNextWhenReady();
+		yield return new WaitUntil (()=> { return Input.GetMouseButtonDown(1) && Input.GetButton("Shift"); }); //wait until yellow
+		audio.autoPlayNext = true;
+
 	}
 }
