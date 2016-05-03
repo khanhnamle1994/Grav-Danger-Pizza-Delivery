4663e288116277fabced834e992934737071001c
Gregory E Garcia <ge95garcia@gmail.com>
2016-05-03 22:32:24 +0200

    First attempt at green arrow

--- Assets/Scripts/PointToGoal.cs
+++ Assets/Scripts/PointToGoal.cs
@@ -6,17 +6,26 @@ public class PointToGoal : MonoBehaviour {
 	public GameObject goal;
 	public GameObject player;
 	SpriteRenderer arrow;
+	Vector3 direction;
 
 	// Use this for initialization
 	void Start () {
 		goal = GameObject.FindGameObjectWithTag ("Finish");
 		player = GameObject.FindGameObjectWithTag ("Player");
 		arrow = gameObject.GetComponent<SpriteRenderer> ();
-		arrow.enabled = false;
+		//arrow.enabled = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		direction = goal.transform.position - player.transform.position;
+		Debug.DrawRay (player.transform.position, direction);
+
+		float angle = Vector2.Angle(goal.transform.position, player.transform.position);
+		Vector3 cross = Vector3.Cross(goal.transform.position, player.transform.position);
+		if (cross.z < 0)
+			angle = 360 - angle;
+
+		gameObject.transform.eulerAngles = new Vector3(0, 0, angle);
 	}
 }
