c8a68f4602c5df5a881f5c708719d3ee6bfd47cf
Gregory E Garcia <ge95garcia@gmail.com>
2016-05-03 23:06:11 +0200

    Started a new Enemy thing

---
+++ Assets/Scripts/Enemy/EnemyHome.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHome : MonoBehaviour {
+
+	public GameObject enemy;
+	GameObject home;
+
+	// Use this for initialization
+	void Start () {
+		home = gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.DrawLine (enemy.transform.position, home.transform.position);
+		Vector2 directionVector = OtherToParticleVector2(other);
+		float gravityForce = home.GetComponent<Rigidbody2D>().mass * .1f;
+		home.GetComponent<Rigidbody2D>().AddForce(directionVector.normalized * force,ForceMode2D.Force);
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.gameObject.tag == "Player")
+		{
+			home = other.gameObject;
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D other) {
+		home = gameObject;
+	}
+}
