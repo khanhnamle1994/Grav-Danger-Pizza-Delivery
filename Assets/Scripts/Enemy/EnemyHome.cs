df605fdbec6e9e27da5ff4e16781c5feac818c5b
Gregory E Garcia <ge95garcia@gmail.com>
2016-05-03 23:10:06 +0200

    Bye bye error

--- Assets/Scripts/Enemy/EnemyHome.cs
+++ Assets/Scripts/Enemy/EnemyHome.cs
@@ -14,9 +14,9 @@ public class EnemyHome : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Debug.DrawLine (enemy.transform.position, home.transform.position);
-		Vector2 directionVector = OtherToParticleVector2(other);
+		/*Vector2 directionVector = OtherToParticleVector2(other);
 		float gravityForce = home.GetComponent<Rigidbody2D>().mass * .1f;
-		home.GetComponent<Rigidbody2D>().AddForce(directionVector.normalized * force,ForceMode2D.Force);
+		home.GetComponent<Rigidbody2D>().AddForce(directionVector.normalized * force,ForceMode2D.Force);*/
 	}
 
 	void OnTriggerEnter2D (Collider2D other) {
