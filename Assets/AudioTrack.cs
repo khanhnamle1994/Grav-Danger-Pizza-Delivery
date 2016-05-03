218098b2044bedd7cbfc824fdfab416150617f8a
kevnburg <kevin.e.ellenburg@vanderbilt.edu>
2016-05-03 19:20:27 +0200

    Audio Stuff - the beginning

---
+++ Assets/AudioTrack.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioTrack : MonoBehaviour {
+
+	public AudioClip[] audioClip;
+
+	// Use this for initialization
+	void Start () {
+		AudioSource audio = GetComponent<AudioSource>();
+		for (int i = 0; i < audioClip.Length; i++) {
+			audio.clip = audioClip [i];
+			audio.Play();
+
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
