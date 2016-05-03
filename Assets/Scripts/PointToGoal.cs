391808f844e337c98c39d8b6531a2b8f45d059d0
Gregory E Garcia <ge95garcia@gmail.com>
2016-05-03 21:05:59 +0200

    Unfinished stuff

---
+++ Assets/Scripts/PointToGoal.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointToGoal : MonoBehaviour {
+
+	public GameObject goal;
+	public GameObject player;
+	SpriteRenderer arrow;
+
+	// Use this for initialization
+	void Start () {
+		goal = GameObject.FindGameObjectWithTag ("Finish");
+		player = GameObject.FindGameObjectWithTag ("Player");
+		arrow = gameObject.GetComponent<SpriteRenderer> ();
+		arrow.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
