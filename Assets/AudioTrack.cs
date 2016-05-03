bb4a16cc482c1ed3f8d31d68d8cf1b576c8be1a2
kevnburg <kevin.e.ellenburg@vanderbilt.edu>
2016-05-03 19:38:47 +0200

    audiotrack progress

--- Assets/AudioTrack.cs
+++ Assets/AudioTrack.cs
@@ -3,20 +3,26 @@ using System.Collections;
 
 public class AudioTrack : MonoBehaviour {
 
-	public AudioClip[] audioClip;
-
+	public AudioClip[] clip;
+	AudioSource audio;
 	// Use this for initialization
 	void Start () {
-		AudioSource audio = GetComponent<AudioSource>();
-		for (int i = 0; i < audioClip.Length; i++) {
-			audio.clip = audioClip [i];
-			audio.Play();
-
-		}
+		audio = GetComponent<AudioSource> ();
+		//StartCoroutine (playAudio());
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		audio.clip = clip[0];
+		audio.Play();
+		yield return WaitUntil (!audio.isPlaying);
+		audio.clip = clip[1];
 	}
+
+	//IEnumerator playAudio() {
+	//	audio.clip = clip[0];
+	//	audio.Play();
+	//	yield return WaitUntil (!audio.isPlaying);
+	//	audio.clip = clip[1];
+	//}
 }
