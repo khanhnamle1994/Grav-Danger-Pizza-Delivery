c9ad92d1f0699524c1e9eb7113505f0c6c479f36
Wesley Lin <wesleylin625@gmail.com>
2016-05-04 13:03:33 +0200

    small base enemey changes

--- Assets/Scripts/Enemy/BaseEnemy.cs
+++ Assets/Scripts/Enemy/BaseEnemy.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public abstract class BaseEnemy : MonoBehaviour {
 
     Rigidbody2D rb2d;
+    public Vector2 homePosition;
+    public float normalSpeed = 5f;
 
 	// Use this for initialization
 	void Start () {
@@ -12,7 +14,7 @@ public abstract class BaseEnemy : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    
 	}
 
     void OnTriggerStay2D(Collider2D other)
@@ -40,16 +42,32 @@ public abstract class BaseEnemy : MonoBehaviour {
     {
         if(col.gameObject.tag=="Player")
         {
-            col.gameObject.GetComponent<PizzaInventory>().DecreaseHealth();
+            CollideWithPlayer(col.gameObject);
         }
     }
 
-    
+    protected abstract void CollideWithPlayer(GameObject other);
 
-    protected void MoveTowards(Vector2 dest)
+    // Move towards destination
+    // with faster speed the farther one is from the desination
+    protected void GravMoveTowards(Vector2 dest)
     {
         Vector2 towardsDest = Vector2Minus(rb2d.position,dest) * 0.7f;
         rb2d.AddForce(towardsDest);
+    }
+
+    protected void MoveTowards(Vector2 dest)
+    {
+        Vector2 towardsDest = Vector2Minus(rb2d.position, dest);
+        towardsDest.Normalize();
+        rb2d.AddForce(towardsDest * normalSpeed);
+    }
+
+    protected void MoveAways(Vector2 dest)
+    {
+        Vector2 towardsDest = Vector2Minus(rb2d.position, dest);
+        towardsDest.Normalize();
+        rb2d.AddForce(towardsDest * normalSpeed * -1f);
     }
 
     Vector2 Vector2Minus(Vector2 beg, Vector2 end)
--- Assets/Scripts/Enemy/EnemyChase.cs
+++ Assets/Scripts/Enemy/EnemyChase.cs
@@ -4,14 +4,38 @@ using System;
 
 public class EnemyChase : BaseEnemy {
 
+
+    private float lastHitPlayerTime = 0f;
+    private bool recentlyDamangedPlayer = false;
+
     protected override void NearPlayerEnemyAI(GameObject other)
     {
-        MoveTowards(other.transform.position);
+        if(!recentlyDamangedPlayer)
+            GravMoveTowards(other.transform.position);
     }
 
     protected override void FarPlayerEnemyAI(GameObject other)
     {
-        Stop();
+        if(!recentlyDamangedPlayer)
+            Stop();
+    }
+
+    protected override void CollideWithPlayer(GameObject other)
+    {
+        
+        if (!recentlyDamangedPlayer)
+        {
+            other.GetComponent<PizzaInventory>().DecreaseHealth();
+            Stop();
+            MoveAways(other.transform.position);
+            recentlyDamangedPlayer = true;
+        }
+    }
+
+    IEnumerator HandleDamage()
+    {
+        yield return new WaitForSeconds(3);
+        recentlyDamangedPlayer = false;
     }
 
 }
