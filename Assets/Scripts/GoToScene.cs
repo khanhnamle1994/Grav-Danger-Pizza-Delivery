75c643082c4bd21698be281941571fced2128385
Gregory E Garcia <ge95garcia@gmail.com>
2016-05-04 14:09:32 +0200

    Intro is done

---
+++ Assets/Scripts/GoToScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor.SceneManagement;
+
+public class GoToScene : MonoBehaviour {
+
+	public int scene;
+
+	public void LoadScene()
+	{
+		EditorSceneManager.LoadScene(scene);
+	}
+}
