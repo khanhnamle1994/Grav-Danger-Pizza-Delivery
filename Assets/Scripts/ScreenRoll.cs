bf31eca45ca39731f2830895cc07d832890a38fb
ballen3434 <benallen1423@gmail.com>
2016-05-05 15:01:25 +0200

    Credit Scene

---
+++ Assets/Scripts/ScreenRoll.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenRoll : MonoBehaviour {
+
+    public GameObject theCamera;
+    public float speed;
+    public string level; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        theCamera.transform.Translate(Vector3.down * Time.deltaTime * speed);
+        StartCoroutine(waitfor());
+
+	
+	}
+
+    IEnumerator waitfor()
+    {
+        yield return new WaitForSeconds(60);
+        Application.LoadLevel(level);
+    }
+}
