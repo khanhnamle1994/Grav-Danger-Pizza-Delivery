9e4f9182fff568a75734065fd4111b0b66a38907
Gregory E Garcia <ge95garcia@gmail.com>
2016-05-05 20:16:17 +0200

    Got rid of outdated code

--- Assets/Scripts/StaticCollideable/Goal.cs
+++ Assets/Scripts/StaticCollideable/Goal.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
-using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
 
 public class Goal : MonoBehaviour {
 
